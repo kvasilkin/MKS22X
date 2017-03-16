9effe41ed26b7c32ca74d637e2605a35a3fa0f12
kvasilkin <kvasilkin@stuy.edu>
2017-03-16 12:32:07 -0400

    bad

--- 07Quick/Partition.java
+++ 07Quick/Partition.java
@@ -8,14 +8,14 @@ public static int[] ex = {7, 1 ,2 ,3,2, 4, 8, 11, 12, 1212, 56454, 3434, 9, 6, 5
 	    public static int  Part (int [] input, int start, int end){
 	int 	 ind = new Random().nextInt(end - start);	
 		int num = input[ind];
-		int temp = 0;
+		//		int temp = 0;
 		int [] hold = new int [(end - start) + 1];
 		//	int beg = start;
 		//	int fin = end;
 	for (int i = start; i < end; i++){
 	    if ( i != ind){
 		if(input[i] < num){
-		    hold[start] = input[i];
+		    hold[i] = input[start + i];
 		    start++;
 
 
@@ -27,15 +27,12 @@ public static int[] ex = {7, 1 ,2 ,3,2, 4, 8, 11, 12, 1212, 56454, 3434, 9, 6, 5
 }
 
 	else{
-	    hold [end - i] =  input[i];
+	    hold [i] =  input[end - i];
 	    end --;
 
 
 
- //	temp = input[end];
-	    //		    input[end] = input[start];
-	    //		    input[start] = temp;	
-
+ 
 	}
 
 	    }
@@ -52,7 +49,7 @@ public static int[] ex = {7, 1 ,2 ,3,2, 4, 8, 11, 12, 1212, 56454, 3434, 9, 6, 5
 		 public static void main(String[] args) {
 		  
 		    
-		     System.out.println(Part(ex, 0, 7));
+		     System.out.println(Part(ex, 0, 10));
 	System.out.println(Arrays.toString(ex));
 		}
 	}
