16967fcbac3cfc3b277826861b8ecea5016e1d06
kvasilkin <kvasilkin@stuy.edu>
2017-03-21 12:26:21 -0400

    added to part

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -7,7 +7,25 @@ public static int[] ex = {7, 1 ,2 ,3,2, 4, 8, 11, 12, 1212, 56454, 3434, 9, 6, 5
 
 	    public static int  Part (int [] input, int start, int end){
 	int 	 ind = new Random().nextInt(end - start);	
-		}
+	int small = start + 1;
+	int big = end;
+	int lt = start + 1;
+	int gt = end;
+	int check = input [ind];
+	swap(input, ind ,lt);
+	for (i = start + 1; i < end; i++){
+	    if (start == end){return start;}
+
+	    if (input[i] > check){
+		swap(input, big, i);
+		big --;
+}
+	    if (input [i] == check){
+		swap(input,lt, i  );
+}
+
+}
+	}
 	    public static void swap (int  [] arr, int i, int j){
 		int hold = arr[i];
 		arr[i] = arr[j];
