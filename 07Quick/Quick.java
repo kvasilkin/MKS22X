485714192acf84adaf2f916c422f6bec108d66b4
kvasilkin <kvasilkin@stuy.edu>
2017-03-21 12:13:10 -0400

    changed class name, added swap

---
+++ 07Quick/Quick.java
@@ -0,0 +1,24 @@
+
+		import java.util.*; 
+		import java.io.*; 
+	public class Quick.java {
+public static int[] ex = {7, 1 ,2 ,3,2, 4, 8, 11, 12, 1212, 56454, 3434, 9, 6, 5};
+	 
+
+	    public static int  Part (int [] input, int start, int end){
+	int 	 ind = new Random().nextInt(end - start);	
+		}
+	    public static void swap (int  [] arr, int i, int j){
+		int hold = arr[i];
+		arr[i] = arr[j];
+		arr[j] = hold;
+}
+
+
+		 public static void main(String[] args) {
+		  
+		    
+		     System.out.println(Part(ex, 0, 10));
+	System.out.println(Arrays.toString(ex));
+		}
+	}
