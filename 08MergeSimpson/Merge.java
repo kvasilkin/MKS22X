5c1d6805e2b9f3ff8d2eabf33b6478cd0c2d619d
kvasilkin <kvasilkin@stuy.edu>
2017-03-23 12:15:23 -0400

    added

--- 08MergeSimpson/Merge.java
+++ 08MergeSimpson/Merge.java
@@ -1,15 +1,32 @@
 public class Merge{
 
 
-def mergesort(ary)   //not enough parameters
-   mergesort the leftHalf
-   mergesort the rightHalf
-   mergeTheTwoHalvesIntoTheOriginalArray
-
-void merge(int[]a,int[]b,int[]destination){...}
-Postcondition:
-     destination contains all of the elements of a and b, and is sorted. 
-Preconditions:
-    a is sorted, b is sorted
-    destination.length == a.length + b.length
+public static void mergesort(int []ary){   //not enough parameters
+    //   mergesort the leftHalf
+    // mergesort the rightHalf
+    //  mergeTheTwoHalvesIntoTheOriginalArray
 }
+   public static void merge(int[]a,int[]b,int[]destination){
+       int i = 0;
+       int j = 0;
+       int l = 0; 
+       while(l < destination.length ){
+	   if(a[i] < b[j]){
+	       destination[l] = a[i];
+	       i++;
+	       l++;
+
+	   }
+
+}
+
+
+
+}
+    //Postcondition:
+    //destination contains all of the elements of a and b, and is sorted. 
+    //Preconditions:
+    // a is sorted, b is sorted
+    //destination.length == a.length + b.length
+}
+
