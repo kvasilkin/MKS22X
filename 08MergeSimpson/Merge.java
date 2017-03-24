705621b13373279c76f8a28d46e0de45d95a92b9
kvasilkin <kvasilkin@stuy.edu>
2017-03-24 10:32:35 -0400

    wowo it still doesnt work

--- 08MergeSimpson/Merge.java
+++ 08MergeSimpson/Merge.java
@@ -1,44 +1,64 @@
+import java.util.*; 
+import java.io.*; 
+
 public class Merge{
 
 
-public static void mergesort(int []ary){   //not enough parameters
-    //   mergesort the leftHalf
-    // mergesort the rightHalf
-    //  mergeTheTwoHalvesIntoTheOriginalArray
-}
-   public static void merge(int[]a,int[]b,int[]destination){
-       int i = 0;
-       int j = 0;
-       int l = 0; 
-       while(l < destination.length ){
-	   if(a[i] < b[j]){
-	       destination[l] = a[i];
-	       i++;
-	       l++;
+    public static void mergesort(int []ary){   //not enough parameters
+	//   mergesort the leftHalf
+	// mergesort the rightHalf
+	//  mergeTheTwoHalvesIntoTheOriginalArray
+    }
+    public static void merge(int[]a,int[]b,int[]destination){
+	int i = 0;
+	int j = 0;
+	int l = 0; 
+	while(l < destination.length &&
+	     i < a.length && j< b.length  ){
+	    if(a[i] < b[j]){
+		destination[l] = a[i];
+		i++;
+		l++;
 
-	   }
-	   if(a[i] > b[j] ){
-	       destination[l] = b[j];
-	       j++;
-	       l++;
-	   }
-	   if(a[i] == b[j]){
-	       destination[l] = a[i];
-	       l++;
-	       i++;
-	       destination[l] = b[j];
-	       l++;
-	       j++;
-}
-}
+	    }
+	    else   if(a[i] > b[j] ){
+		destination[l] = b[j];
+		j++;
+		l++;
+	    }
+	    else   if(a[i] == b[j]){
+		destination[l] = a[i];
+		l++;
+		i++;
+		destination[l] = b[j];
+		l++;
+		j++;
+	    }
+	    if(a.length > b.length){
+		for(int k = a.length  - b.length - 1; k < a.length; k++){
+		    destination[l] = a[k];
+		}}
+	
+    if(b.length > a.length){
+		for(int k = b.length  - a.length - 1 ; k < b.length; k++){
+		    destination[l] = b[k];}}
+	}
+
+    }
 
 
 
-}
-    //Postcondition:
-    //destination contains all of the elements of a and b, and is sorted. 
-    //Preconditions:
-    // a is sorted, b is sorted
-    //destination.length == a.length + b.length
+    public static void main(String[] args){
+	int [] Karl = {1, 3,  5, 7, 9, 11, 13 };
+	int [] Marx = {2, 4, 6, 8, 10 };
+	int[] dest = new int[Karl.length + Marx.length];
+	merge (Karl, Marx, dest);
+	System.out.println(Arrays.toString(dest));
+
+    }
+
+
+
+    
 }
 
