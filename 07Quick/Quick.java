49e9538440d7e34e27a8df2c3a31bf97005bdac9
kvasilkin <kvasilkin@stuy.edu>
2017-03-24 01:10:16 -0400

    working dutch flag

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -6,12 +6,17 @@
 	 public  static int [] ex;
 
 	    public static int  Part (int [] input, int start, int end){
-	int 	 ind = start + new Random().nextInt(end - start);	
+if (start == end) {
+	    return start;
+	}
+
+	int ind = start + new Random().nextInt(end - start);
 	int lt = start ;
-	int gt = end;
+	int gt = end -1 ;
 	int check = input [ind];
-	int i = start;
-while (i < gt){
+	int i = start + 1;
+	swap(input, lt, ind);
+while (i <= gt){
 if (input[i] == check ){
 			i ++;
 						}
@@ -24,12 +29,15 @@ else if  (input[i] < check){
 else {
 	swap (input, i, gt);
 	gt--;
+	
 }
 
 	
 
 	}
-return ind;}
+return lt;}
+
+
 	    public static void swap (int  [] arr, int i, int j){
 		int hold = arr[i];
 		arr[i] = arr[j];
@@ -38,13 +46,15 @@ return ind;}
 
 
 		 public static void main(String[] args) {
-		  int len = 20;
+		 
+		 int[] ary = {9,8,8,8,7,7,7,6,5,4,4,4,3,2,1,1,1,0};
+		  int len = 30;
 		  ex = new int [len];
 		  for (int i = 0; i < len - 1; i ++){
-ex [i] = new Random().nextInt(5);	
+ex [i] = new Random().nextInt(7);	
 		  }
 		    
-		     System.out.println(Part(ex, 0, len -1 ));
-	System.out.println(Arrays.toString(ex));
+		     System.out.println(Part(ary, 0, ary.length  ));
+	System.out.println(Arrays.toString(ary));
 		}
 	}
