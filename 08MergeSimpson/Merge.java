9c5f7ba46b510bff18b6f76bea8280f1513a5c84
kvasilkin <kvasilkin@stuy.edu>
2017-04-03 00:11:28 -0400

    Done. needs more testing

--- 08MergeSimpson/Merge.java
+++ 08MergeSimpson/Merge.java
@@ -4,58 +4,61 @@ import java.io.*;
 public class Merge{
 
 
-    public static void mergesort(int []ary){   //not enough parameters
-	//   mergesort the leftHalf
-	// mergesort the rightHalf
-	//  mergeTheTwoHalvesIntoTheOriginalArray
+    public static void mergesort(int []ary){  
+if(ary.length < 2){
+	return;
+}
+
+int []  left = new int[ary.length / 2];
+int [] right = new int[ary.length - left.length];
+
+for(int k = 0; k < left.length; k++){
+	left[k] = ary[k];
+} 
+
+for(int k = 0; k < right.length; k++){
+	right[k] = ary[left.length + k];
+}
+
+mergesort(left);
+mergesort(right);
+merge(left, right, ary);
     }
+
+
+
     public static void merge(int[]a,int[]b,int[]destination){
 	int i = 0;
 	int j = 0;
-	int l = 0; 
-	while(l < destination.length &&
-	     i < a.length && j< b.length  ){
-	    if(a[i] < b[j]){
-		destination[l] = a[i];
-		i++;
-		l++;
-
-	    }
-	    else   if(a[i] > b[j] ){
-		destination[l] = b[j];
-		j++;
-		l++;
-	    }
-	    else   if(a[i] == b[j]){
-		destination[l] = a[i];
-		l++;
-		i++;
-		destination[l] = b[j];
-		l++;
-		j++;
-	    }}
-	      if(a.length > b.length){
-		for(int k = b.length; k < a.length; k++){
-		    destination[l] = a[k];
-		}}
-	
-    if(b.length > a.length){
-		for(int k =  a.length; k < b.length; k++){
-		destination[l] = b[k];}}
-
-
-	
-
+	while(i <= a.length && j <= b.length && i + j < destination.length ){
+	    if (i == a.length){
+			destination[i +j] = b[j];
+			j++;
+	  			  }
+	    else if (j == b.length){
+			destination[i + j] = a[i];
+			i++;
+	  			  }
+	    else if (a[i] <= b[j]){
+			destination[i + j] = a[i];
+			i++;
+	   			 }
+	    else if(b[j] < a[i]){
+			destination[i + j] = b[j];
+			j++;	    }
+	}
     }
 
 
 
     public static void main(String[] args){
-	int [] Karl = {1, 3,  5, 7, 9, 11, 13 };
-	int [] Marx = {2, 4, 6, 8, 10 };
-	int[] dest = new int[Karl.length + Marx.length];
-	merge (Karl, Marx, dest);
-	System.out.println(Arrays.toString(dest));
+	int [] Karl = { 13 };
+	int [] Marx = {23, 23, 23, 1, 2, 12, 323, 12,  2324,32,1, 2, 1, 1, 1, 1,1 ,2, 2, 2, 3, 2, 1, 1, 1, 2, 3, 123, 3 ,2 };
+//	System.out.println(Karl.length);
+//	int[] dest = new int[Karl.length + Marx.length];
+//	merge (Karl, Marx, dest);
+mergesort(Marx);
+	System.out.println(Arrays.toString(Marx));
 
     }
 
@@ -63,4 +66,3 @@ public class Merge{
 
     
 }
-
