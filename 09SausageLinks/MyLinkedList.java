5d90d5c3fbb439bf61731eaf3e05d422848c4284
kvasilkin <kvasilkin@stuy.edu>
2017-04-05 12:08:11 -0400

    Add files via upload

--- 09SausageLinks/MyLinkedList.java
+++ 09SausageLinks/MyLinkedList.java
@@ -1,7 +1,47 @@
+import java.util.*;
 public class MyLinkedList{
-LNode start;
-            int size;
-    class LNode{
 
-}
+  private class LNode{
+    LNode next,prev;
+    int value;
+    public LNode(int value){
+      this.value=value;
+    }
+    public String toString(){
+      return value+"";
+    }
+  }
+
+  LNode head,tail;
+  int size;
+
+  public MyLinkedList(){
+  }
+
+  public int size(){
+    return size;
+  }
+
+    
+  private LNode getNthNode(int n){/*complete this*/}
+
+  private void addAfter(LNode location, LNode toBeAdded){/*complete this*/  }
+
+  private void remove(LNode target){/*complete this*/}
+
+  public String toString(){ /*complete this*/  }
+
+  public boolean add(int value){  /*complete this*/ }
+
+  public int get(int index){/*complete this*/}
+
+  public int set(int index, int value){/*complete this*/}
+
+  public int indexOf(int value){/*complete this*/}
+
+  public int remove(int index){/*complete this*/}
+
+  public void add(int index,int value){/*complete this*/}
+
+
 }
