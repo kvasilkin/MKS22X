c31dd2e6bded686c153f4137cdbfeee7b4da9d74
kvasilkin <kvasilkin@stuy.edu>
2017-05-03 12:27:17 -0400

    Add files via upload

---
+++ 11Postfic/Postfix.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Postfix{
+    public static eval(String s){
+	int tokens[] = s.split(" ");
+	nums = new stack;
+
+}
+    public static isOP(String str ){
+
+}
+
+    public static apply(String op, String a, String b){
+
+}
+}
