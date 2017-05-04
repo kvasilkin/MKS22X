29b67a4171d5458ff64430afea585c12f3c36def
kvasilkin <kvasilkin@stuy.edu>
2017-05-04 12:01:52 -0400

    skelly

---
+++ 13heapsofFun/MyHeap.java
@@ -0,0 +1,25 @@
+Public Class MyHeap{
+public static ArrayList<String> holder = new ArrayList<String>();
+
+    public MyHeap(){
+
+}
+    // - construct empty max heap
+    public  MyHeap(boolean){}
+    // - true: construct empty max heap, false: construct empty min heap.
+    //Methods
+
+
+
+    public void add(String s){}
+
+public  String remove(){}
+
+    public String peek(){}
+
+    //Private methods:
+
+    public  void pushUp(){}
+
+public void pushDown(){}
+}
