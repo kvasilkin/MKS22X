fc4b29ff02ec506227c8924b4dd6ca09662d7dfb
kvasilkin <kvasilkin@stuy.edu>
2017-05-04 12:26:27 -0400

    Add files via upload

--- 13heapsofFun/MyHeap.java
+++ 13heapsofFun/MyHeap.java
@@ -1,25 +1,55 @@
 Public Class MyHeap{
-public static ArrayList<String> holder = new ArrayList<String>();
-
+    public static ArrayList<String> holder;
+    public static boolean dir; 
     public MyHeap(){
-
+holder =  new ArrayList<String>();
+dir = true;
 }
     // - construct empty max heap
-    public  MyHeap(boolean){}
+    public  MyHeap(boolean in){
+holder =  new ArrayList<String>();
+dir = in;
+}
     // - true: construct empty max heap, false: construct empty min heap.
     //Methods
 
 
 
-    public void add(String s){}
+    public void add(String s){
+	int n = holder.size();
+	holder.add(n, s);
+	pushUp(n);
 
-public  String remove(){}
+}
 
-    public String peek(){}
+    public  String remove(){
+    
+	int n =    holder.size();    
+	String	result = holder.get(1);
+	swap(1, n);
+	holder.remove(n);
+	pushDown(1);   
+	return result;
+
+}
+
+    public String peek(){
+	return holder.get(1);
+}
 
     //Private methods:
+    //possibly swap
+    public  void pushUp(int ind){
 
-    public  void pushUp(){}
+}
 
-public void pushDown(){}
+public void pushDown(int ind){}
+
+
+public void swap(int first, int second){
+    int temp = holder.get(first);
+    holder.set(first, second);
+    holder.set(second, temp);
+
+}
 }
