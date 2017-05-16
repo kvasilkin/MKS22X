3df63ca2b3643f5c0c58ad7b400dd51427693c9d
kvasilkin <kvasilkin@stuy.edu>
2017-05-16 12:25:42 -0400

    location

--- 15aMAZEment/Location.java
+++ 15aMAZEment/Location.java
@@ -1,3 +1,35 @@
-public class Location{
+public class Location implements Comparable<Location>{
+    private int row,col;
+     private int distToGoal;
+     private int distToStart; 
+     private Location previous; //(used to trace the solution)
+     private boolean aStar;// : when this is true, compareTo will use: previousDistance + distanceToGoal
+     //   when this is false, compareTo will use the distanceToGoal only.}
+     public void Location(int r, int c, Location prev, int toStart, int toGoal, boolean AStar){
+	 row = r;
+	 col = c;
+	 previous = prev;
+	 distToStart =toStart;
+	 distToGoal = toGoal;
+	 aStar = AStar;
+
+}
+     public int  CompareTo( Location other){ //-> decide what number to compare using the aStar boolean.
+	 if(aStar){
+return ((this.distanceToGoal + this.distanceToStart) - ( other.distanceToGoal + other.distanceToStart));
+}
+	 else{
+	     return ( this.distanceToGoal - other.distanceToGoal);}
+	     
+     }
+
+
+
+
+
+
+
+
+
 
 }
